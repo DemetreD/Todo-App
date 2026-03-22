c2932fb57a56e4f7d166c06a148459e2cf56262c
Demetre <136166143+DemetreD@users.noreply.github.com>
2026-03-22 20:58:41 +0400

    add overview items and icons for sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Clock, Calendar, LayoutPanelLeft } from 'lucide-angular';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  readonly icons = {
+    AllTasks: LayoutPanelLeft,
+    Clock: Clock,
+    Calendar: Calendar,
+  }
 }
